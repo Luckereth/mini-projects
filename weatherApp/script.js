54e84caf8d6f86d746860966b52a96c1a8f9aacb
Luckereth <hlavinkalukas@email.cz>
2024-12-15 15:20:05 +0100

    url repaired + added Celsius units

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -6,7 +6,7 @@ let clouds = document.getElementById("clouds");
 let humidity = document.getElementById("humidity");
 let pressure = document.getElementById("pressure");
 let form = document.getElementById("inputForm");
-let main= document.querySelector('main')
+let main = document.querySelector('main')
 
 form.addEventListener("submit", (event) => {
     event.preventDefault()
@@ -15,8 +15,10 @@ form.addEventListener("submit", (event) => {
     }
 })
 let id = "5713576368d7d910197dfccf2efcfefa"
-let url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appdi='+id;
 function searchWeather() {
+
+    let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchInput.value + ',&APPID=' + id+'&units=metric';
+
     fetch(url)
         .then(responsive => responsive.json())
         .then(data => {
@@ -24,25 +26,25 @@ function searchWeather() {
             if (data.cod == 200) {
                 city.querySelector("figcaption").innerText = data.name;
                 city.querySelector("img").src = "https://flagsapi.com/" + data.sys.country + "/shiny/32.png"
-                temperature.querySelector('img').src='http://openweathermap.org/img/wn/'+data.weather[0].icon+'@4x.png';
-                temperature.querySelector('figcaption span').innerText=data.main.temp;
-                description.innerText=data.weather[0].description;
-                clouds.innerText=data.clouds.all;
-                humidity.innerText=data.main.humidity;
-                pressure.innerText=data.main.pressure;
-            }else{
+                temperature.querySelector('img').src = 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '@4x.png';
+                temperature.querySelector('figcaption span').innerText = data.main.temp;
+                description.innerText = data.weather[0].description;
+                clouds.innerText = data.clouds.all;
+                humidity.innerText = data.main.humidity;
+                pressure.innerText = data.main.pressure;
+            } else {
                 //false
                 main.classList.add('error')
                 setTimeout(() => {
-                   main.classList.remove('error') 
+                    main.classList.remove('error')
                 }, 1000);
             }
 
-            searchInput.value='';
+            searchInput.value = '';
         })
 }
 function initApp() {
-    searchInput.value='Praha';
+    searchInput.value = 'Praha';
     searchWeather();
 }
 
